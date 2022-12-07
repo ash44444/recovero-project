db8f5feae13cd24696ae360f721c8b91e050c670
AshishManer <ashishmaner94@gmail.com>
2022-12-07 16:57:51 +0530

    first commit

--- server.js
+++ server.js
@@ -1,43 +1,54 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const users = require('./routes/api/users');
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const users = require('./routes/api/users')
 
-require('./config/passport')(passport);
+require('./config/passport')(passport)
 
-const app = express();
+const app = express()
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.listen(9000);
+app.listen(9000)
 
-const db = require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI
 
-mongoose.connect(db, { useNewUrlParser: true })
-    .then(() =>
-        console.log('MongoDB successfully connected.')
-    ).catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB successfully connected.'))
+  .catch((err) => console.log(err))
 
-app.use(passport.initialize());
+app.use(passport.initialize())
 
-app.use('/api', users);
+app.use('/api', users)
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+if (process.env.NODE_ENV == 'production') {
+  const path = require('path')
+  app.get('/', (req, res) => {
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`))
