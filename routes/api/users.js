7819ac19e9a56e260a45b115c571309f6b9fd3d9
AshishManer <108808771+ash44444@users.noreply.github.com>
2022-12-13 12:44:21 +0530

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -134,5 +134,36 @@ router.post('/login', (req, res) => {
     })
   })
 })
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body)
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' })
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+          newUser
+            .save()
+            .then((user) => {
+              return res.status(200).json({
+                message: 'User added successfully. Refreshing data...',
+              })
+            })
+            .catch((err) => console.log(err))
+        })
+      })
+    }
+  })
+})
 
 module.exports = router
